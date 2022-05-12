3d8ca7b2160791996357d45da060b51a3f196ef0
mendycohen <mendy.cohen@projectinflection.com>
2022-05-12 10:35:12 -0400

    test for scrubData function works to check for digits

--- scrubData.ts
+++ scrubData.ts
@@ -8,8 +8,10 @@ export type hardDrive = {
 
 export default function scrubData( items: hardDrive[] ) {
   const scrubbedItems: Array<scrubbedHardDrive> = items.map((item: hardDrive) => {
-    const scrubbedDollars = (parseInt(item.itemDollars) * 100);
-    const scrubbedCents = parseInt(item.itemCents);
+    let scrubbedDollars = (parseInt(item.itemDollars) * 100);
+    /^\d+$/.test(item.itemDollars) === true ? scrubbedDollars : scrubbedDollars = 0;
+    let scrubbedCents  = parseInt(item.itemCents);
+    /^\d+$/.test(item.itemCents) === true ? scrubbedCents : scrubbedCents = 0;
     let price = scrubbedDollars + scrubbedCents;
     return {
       itemName: item.itemName,
@@ -29,8 +31,8 @@ console.log(
   },
   {
     itemName: 'asd',
-    itemDollars: '100',
-    itemCents: '00',
+    itemDollars: 'jj',
+    itemCents: '08',
   },
   {
     itemName: 'asd',
--- tryout.js
+++ tryout.js
@@ -1,5 +1,13 @@
-let str = "11";
-str2 = str.slice("");
-console.log(typeof(str2));
-console.log(typeof(parseInt(str)));
-console.log(typeof(parseInt("")))
+// let str = "11";
+// str2 = str.slice("");
+// console.log(typeof(str2));
+// console.log(typeof(parseInt(str)));
+// console.log(typeof(parseInt("")))
+
+
+// var str = "Amount is 1000";
+// var num = +str.replace(/[^0-9.]/g,"");
+// console.log(typeof num);
+let val = "999.99"
+let isnum = /^\d+$/.test(val);
+console.log(isnum);
